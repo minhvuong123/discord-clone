70b6c704edd54720606d3e970a579783b4df0118
minhvuong123 <minhvuong12222@gmail.com>
2024-02-24 01:31:44 +0700

    initial profile by clerk data

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,23 @@
+import { initialProfile } from "@/lib/inital-profile";
+import { prismadb } from "@/lib/prismadb";
+import { redirect } from "next/navigation";
+
 const SetupPage = async () => {
+  const profile = await initialProfile();
+  const server = await prismadb.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  })
+
+  if (server) {
+    return redirect(`/server/${server.id}`)
+  }
+
   return (
     <div>
       Create a Server
